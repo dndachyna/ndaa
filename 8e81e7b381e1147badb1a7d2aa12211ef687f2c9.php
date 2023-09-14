46875f65461d22fc527701fef0bc7e4dcf244771
dndachyna <143949168+dndachyna@users.noreply.github.com>
2023-09-15 01:17:19 +0700

    Add files via upload

---
+++ 8e81e7b381e1147badb1a7d2aa12211ef687f2c9.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prjct Boxity</title>
+    <link rel="stylesheet" href="<?php echo e(asset('style.css')); ?>">
+</head>
+<body>
+    <div class="wrapper">
+        <div class="nav">
+            <div class="logo">
+                <a href="#">
+                    <p>Mr Seblak</p>
+                </a>
+            </div>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Menu</li>
+                <li>Contact Us</li>
+            </ul>
+        </div>
+     </div>
+</body>
+</html><?php /**PATH C:\laragon\www\blazingTeam\resources\views/welcome.blade.php ENDPATH**/ ?>
